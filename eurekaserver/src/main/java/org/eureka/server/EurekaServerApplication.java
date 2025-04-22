247cb682c7b49200d5985323c40255e898acae6e
Dragon <626571494@qq.com>
2025-04-22 21:32:39 +0800

    springcloud [eureka server client]

---
+++ eurekaserver/src/main/java/org/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package org.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @auther yangxiaolong
+ * @create 2025/3/27
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
